64a4362523dec321614b114efc696005aeddd4f3
difei-hub <difei1113@gmail.com>
2023-05-24 08:42:35 -0400

    d

--- containers.py
+++ containers.py
@@ -1,5 +1,6 @@
 import json
 import random
+import subtraction
 data = open("test.json", encoding="utf8")
 chest = json.load(data)
 data2 = open("test2.json", encoding="utf8")
@@ -7,9 +8,23 @@ loot = json.load(data2)
 
 
 key_list = []
-x = random.uniform(0,100)
-for i in chest:
-    for i in i["items"]:
-        for k in i:
-            key_list.append(k)
-print(key_list)
+def find_Item():
+    bob = ""
+    x = random.uniform(0,100)
+    print(x)
+    if x - chest[0]["#Poor"] <= 0:
+        bob = "Poor"
+        item = random.choice(chest[0]["#Poor_Items"])
+        print(item)
+        print(bob)
+    elif x - chest[0]["#Common"] > 0:
+        bob = "Common"
+        item = random.choice(chest[0]["#Common_Item"])
+        print(item)
+        print(bob)
+
+    
+
+
+find_Item()
+
---
+++ subtraction.py
@@ -0,0 +1,13 @@
+import json
+import random
+data = open("test.json", encoding="utf8")
+chest = json.load(data)
+data2 = open("test2.json", encoding="utf8")
+loot = json.load(data2)
+x = random.uniform(0,100)
+chance_1 = x - chest[0]["#Poor"]
+chance_2 = chance_1 - chest[0]["#Common"]
+chance_3 = chance_2 - chest[0]["#Uncommon"]
+chance_4 = chance_3 - chest[0]["#Rare"]
+chance_5 = chance_4 - chest[0]["#Epic"]
+chance_6 = chance_5 - chest[0]["#Legendary"]
