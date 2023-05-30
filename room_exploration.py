fb93dcfcd8c0c0bc6fabe7e499b3383d7fad1534
difei-hub <difei1113@gmail.com>
2023-05-30 16:01:21 -0400

    fixed "NOne problem"

--- room_exploration.py
+++ room_exploration.py
@@ -1,10 +1,9 @@
-interaction_object = ""
+
 import json 
 import random
 x = open("Crypt_02_HR.json")
 map = json.load(x)
 """from room_select import current_room"""
-room_status = " "
 y = random.randint(0,len(map[0]["spawns"])-1)
 current_room = map[0]["spawns"][y]
 print(f"You are currently in room {current_room}")
@@ -17,15 +16,17 @@ while room_find != current_room:
     room_find = map[room_find_num]["room"]
 mobs = map[room_find_num]["monster"]
 print(mobs)
-mob =  input(print("which mob do you want to fight right now?"))
+current_mob = input(("which mob do you want to fight right now?"))
+print(current_mob)
 
 def fight():
-    if mob in mobs:
+    if current_mob in mobs:
         print("hi")
     else: 
         print("Invalid mob. Try again")
 
 fight()
+"""room_status = " "
 if room_status == "cleared":
     room_find_num = 1
     room_find = map[room_find_num]["room"]
@@ -49,29 +50,4 @@ if room_status == "completed":
             room_select = False
         else:
             print("Invalid room. Try again")
-
-
-
-
-
-
 """
-y = random.randint(0,len(map[0]["spawns"])-1)
-current_room = map[0]["spawns"][y]
-print(f"You are currently in room {current_room}")
-
-room_find_num = 1
-room_find = map[room_find_num]["room"]
-while room_find != current_room:
-    room_find_num = room_find_num + 1
-    room_find = map[room_find_num]["room"]
-connection = map[room_find_num]["connection"]
-
-room_select = True
-while room_select == True:
-    test = input(f"What room do you want to go to next? {connection} ")
-    if test in connection:
-        current_room = test
-        room_select = False
-    else:
-        print("Invalid room. Try again")"""
